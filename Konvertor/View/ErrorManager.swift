50b704081f00f48a469ac8cc00769ec9fb8a1299
mrsSwift <mrsSwift@mrMac.local>
2019-01-24 00:04:38 +0200

    Add exchange api in project. Files:
    APICurrencyManager
    APIManager
    CurrentCurrency
    ErrorManager
    edit InitialVC (api_key and demo)

---
+++ Konvertor/View/ErrorManager.swift
@@ -0,0 +1,13 @@
+//
+//  ErrorManager.swift
+//  Konvertor
+//
+//  Created by mrsSwift on 23.01.2019.
+//  Copyright © 2019 Perchiki. All rights reserved.
+//
+
+import Foundation
+
+public let PerchikiNetworkingErrorDomain = "PerchikiNetworkingError"
+public let MissingHTTPResponseError = 100
+public let UnexpectedResponseError = 200
